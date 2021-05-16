5ef4d8f07a0aae6969216b169e106875a2293944
mvallejos3100 <46535390+mvallejos3100@users.noreply.github.com>
2021-05-15 18:16:35 -0600

    Update general.py

--- general/general.py
+++ general/general.py
@@ -5,7 +5,7 @@ rele = Pin(16, Pin.OUT)
 
 while True:
   rele.on()
-  time.sleep(1)
+  time.sleep(.5)
   rele.off()
-  time.sleep(1)
+  time.sleep(.5)
   print('Versión actualizada por medio de OTA\n') 
